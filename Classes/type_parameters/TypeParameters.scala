9fa8a12d5042acdbdae8bbce06265c9a6703b2c3
Angelos <angelos.kapsimanis@gmail.com>
2014-06-15 00:41:37 +0200

    Type parameters

--- Classes/type_parameters/TypeParameters.scala
+++ Classes/type_parameters/TypeParameters.scala
@@ -12,19 +12,14 @@ object test {
     else nth(n-1, xs.tail)            //> nth: [T](n: Int, xs: lists.List[T])T
 
   val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
-                                                  //> list  : lists.Cons[Int] = lists.Cons@61ec56c9
+                                                  //> list  : lists.Cons[Int] = lists.Cons@46727249
   nth(1,list)                               //> res0: Int = 2
-  nth(-1, list)                             //> java.lang.IndexOutOfBoundsException
-                                                  //|   at lists.test$$anonfun$main$1.nth$1(lists.test.scala:9)
-                                                  //|   at lists.test$$anonfun$main$1.apply$mcV$sp(lists.test.scala:15)
-                                                  //|   at org.scalaide.worksheet.runtime.library.WorksheetSupport$$anonfun$$exe
-                                                  //| cute$1.apply$mcV$sp(WorksheetSupport.scala:76)
-                                                  //|   at org.scalaide.worksheet.runtime.library.WorksheetSupport$.redirected(W
-                                                  //| orksheetSupport.scala:65)
-                                                  //|   at org.scalaide.worksheet.runtime.library.WorksheetSupport$.$execute(Wor
-                                                  //| ksheetSupport.scala:75)
-                                                  //|   at lists.test$.main(lists.test.scala:5)
-                                                  //|   at lists.test.main(lists.test.scala)
+}
+
+object List {
+  def apply[T]() = new Nil
+  def apply[T](x1: T): List[T] = new Cons(x1, new Nil)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
 }
 
 trait List[T] {
