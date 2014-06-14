10935fd9a52ac286d19b45568d62fabc3ab0ca79
Angelos <angelos.kapsimanis@gmail.com>
2014-06-14 17:50:29 +0200

    rationals OO

---
+++ Classes/higher_order_funs/rationals.scala
@@ -0,0 +1,56 @@
+package rational
+
+object classes {
+  val r = new Rational(1,2)                 //> r  : rational.Rational = 1/2
+  r.numer                                   //> res0: Int = 1
+  r.denom                                   //> res1: Int = 2
+
+  def addRational(r: Rational, s: Rational): Rational =
+    new Rational(r.numer * s.denom + s.numer * r.denom,
+      r.denom * s.denom)        //> addRational: (r: rational.Rational, s: rational.Rational)rational.Rational
+
+
+  def makeString(r: Rational) =
+    r.numer + "/" + r.denom           //> makeString: (r: rational.Rational)String
+
+  makeString(addRational(new Rational(1,2), new Rational(2,3)))
+                                                  //> res2: String = 7/6
+  r.add(new Rational(2,3))                        //> res3: rational.Rational = 7/6
+
+  val x = new Rational(1,3)                 //> x  : rational.Rational = 1/3
+  val y = new Rational(5,7)                 //> y  : rational.Rational = 5/7
+  val z = new Rational(3,2)                 //> z  : rational.Rational = 3/2
+  x.sub(y).sub(z)                           //> res4: rational.Rational = -79/42
+  z.less(x)                                 //> res5: Boolean = false
+  x.max(z)                                  //> res6: rational.Rational = 3/2
+
+}
+
+class Rational(x: Int, y: Int) {
+  //require(y > 0, "denominator must be positive")
+  def this(x: Int) = this(x,1)
+
+  val numer = x
+  val denom = y
+
+  def add(that: Rational) =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
+
+  def sub(that: Rational) =
+    this.add(that.neg)
+
+  def neg() =
+    new Rational(-numer, denom)
+
+  def less(that: Rational) = numer * that.denom < that.numer * denom
+
+  def max(that: Rational) = if(this.less(that)) that else this
+
+  override def toString = {
+    def gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a % b)
+    val g = gcd(numer, denom)
+    numer / g  +  "/" + denom / g
+  }
+}
