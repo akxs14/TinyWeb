74efffa704acee6d41135918979813cc42d41b6a
Angelos <angelos.kapsimanis@gmail.com>
2014-06-14 20:40:28 +0200

    rationals OO

--- Classes/higher_order_funs/rationals.scala
+++ Classes/higher_order_funs/rationals.scala
@@ -15,14 +15,14 @@ object classes {
 
   makeString(addRational(new Rational(1,2), new Rational(2,3)))
                                                   //> res2: String = 7/6
-  r.add(new Rational(2,3))                        //> res3: rational.Rational = 7/6
+  r + new Rational(2,3)                           //> res3: rational.Rational = 7/6
 
   val x = new Rational(1,3)                 //> x  : rational.Rational = 1/3
   val y = new Rational(5,7)                 //> y  : rational.Rational = 5/7
   val z = new Rational(3,2)                 //> z  : rational.Rational = 3/2
-  x.sub(y).sub(z)                           //> res4: rational.Rational = -79/42
-  z.less(x)                                 //> res5: Boolean = false
-  x.max(z)                                  //> res6: rational.Rational = 3/2
+  x - y - z                                 //> res4: rational.Rational = -79/42
+  z < x                                     //> res5: Boolean = false
+  x max z                                   //> res6: rational.Rational = 3/2
 
 }
 
@@ -33,20 +33,20 @@ class Rational(x: Int, y: Int) {
   val numer = x
   val denom = y
 
-  def add(that: Rational) =
+  def + (that: Rational) =
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom)
 
-  def sub(that: Rational) =
-    this.add(that.neg)
+  def - (that: Rational) =
+    this + -that
 
-  def neg() =
+  def unary_- : Rational =
     new Rational(-numer, denom)
 
-  def less(that: Rational) = numer * that.denom < that.numer * denom
+  def < (that: Rational) = numer * that.denom < that.numer * denom
 
-  def max(that: Rational) = if(this.less(that)) that else this
+  def max(that: Rational) = if(this < that) that else this
 
   override def toString = {
     def gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a % b)
